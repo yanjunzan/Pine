32669673457a43e36b0c4875e5bf7ef76988d502
yanjunzan <yanjunzan@gmail.com>
2020-10-06 13:48:47 +0200

    filter ref for 1mb

---
+++ bin/201006_filter_fastq_min_len.pl
@@ -0,0 +1,21 @@
+# filterfastarange.pl perl filterfastarange.pl 301 600 contigs.fasta > contigs-gt300-lte600.fasta
+#perl filterfastarange.pl 301 600 contigs.fasta > contigs-gt300-lte600.fasta
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $minlen = shift or die "Error: `minlen` parameter not provided\n";
+#my $maxlen = shift or die "Error: `maxlen` parameter not provided\n";
+{
+    local $/=">";
+    while(<>) {
+        chomp;
+        next unless /\w/;
+        s/>$//gs;
+        my @chunk = split /\n/;
+        my $header = shift @chunk;
+        my $seqlen = length join "", @chunk;
+        print ">$_" if($seqlen >= $minlen );
+    }
+    local $/="\n";
+}
